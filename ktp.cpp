ef14cd1479ee085bf531e81dd9d66cdffacab148
Kuma dayo <57862189+inikumadayo@users.noreply.github.com>
2025-04-15 16:19:03 +0700

    Update ktp.cpp

--- ktp.cpp
+++ ktp.cpp
@@ -2,12 +2,25 @@
 using namespace std;
 
 int main(){
-	long long int nik, tanggal,rt,rw;
-	string nama, tempat, kelamin, alamat, kelurahan, kecamatan, agama, status, pekerjaan, keluarga, berlaku hingga;
+	long long int nik, tanggal, rt, rw, bulan, tahun;
+	string nama, tempat, kelamin, alamat, kelurahan, kecamatan, agama, status, pekerjaan, kewarganegaraan, berlaku;
 	
-	cout<<"============KTP============"<<endl;
-	cout<<"NIK      : "; cin>>nik;
-	cout<<"nama     : "; cin>>nama;
-	cout<<"Tempat   : "; cin>>tempat;
-	cout<<"tanggal  : "; cin>>nama;
+	cout<<"====================Input KTP===================="<<endl;
+	cout<<"NIK		: "; cin>>nik;
+	cout<<"Nama		: "; cin>>nama;
+	cout<<"Tempat lahir	: "; cin>>tempat;
+	cout<<"Tanggal lahir	: "; cin>>tanggal;
+	cout<<"Bulan lahir	: ";cin>>bulan;
+	cout<<"Tahun lahir	: ";cin>>tahun;
+	cout<<"Jenis kelamin	: ";cin>>kelamin;
+	cout<<"Dusun/Kampung	: ";cin>>alamat;
+	cout<<"RT		: ";cin>>rt;
+	cout<<"RW		: ";cin>>rw;
+	cout<<"Kelurahan/Desa	: ";cin>>kelurahan;
+	cout<<"Kecamatan	: ";cin>>kecamatan;
+	cout<<"Agama		: ";cin>>agama;
+	cout<<"Status Perkawinan: ";cin>>status;
+	cout<<"Pekerjaan	: ";cin>>pekerjaan;
+	cout<<"Kewarganegaraan	: "cin>>kewarganegaraan;
+	cout<<"Berlaku hingga	: ";cin>>berlaku;
 }
