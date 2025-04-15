fa34086b71a92ff8957f6cad43bbf07f90ab4363
Sorashiro <rafisatyaprayoga@gmail.com>
2025-04-15 16:32:53 +0700

    Update ktp.cpp

--- ktp.cpp
+++ ktp.cpp
@@ -18,29 +18,6 @@ int main(){
 	cout<<"RW			: ";cin>>rw;
 	cout<<"Kelurahan/Desa		: ";cin>>kelurahan;
 	cout<<"Kecamatan		: ";cin>>kecamatan;
-	cout<<"Agama			: ";cin>>agama; cin.ignore();
-	cout<<"Status Perkawinan	: ";getline(cin,status);
-	cout<<"Pekerjaan		: ";cin>>pekerjaan;
-	cout<<"Kewarganegaraan		: ";cin>>kewarganegaraan; #include <iostream>
-using namespace std;
-
-int main(){
-	long long int nik, tanggal, rt, rw, bulan, tahun;
-	string nama, tempat, kelamin, alamat, kelurahan, kecamatan, agama, status, pekerjaan, kewarganegaraan, berlaku;
-
-	cout<<"====================Input KTP===================="<<endl;
-	cout<<"NIK			: ";cin>>nik; cin.ignore();
-	cout<<"Nama			: ";getline(cin,nama);
-	cout<<"Tempat lahir		: ";cin>>tempat;
-	cout<<"Tanggal lahir		: ";cin>>tanggal;
-	cout<<"Bulan lahir		: ";cin>>bulan;
-	cout<<"Tahun lahir		: ";cin>>tahun;
-	cout<<"Jenis kelamin		: ";cin>>kelamin;
-	cout<<"Dusun/Kampung		: ";cin>>alamat;
-	cout<<"RT			: ";cin>>rt;
-	cout<<"RW			: ";cin>>rw;
-	cout<<"Kelurahan/Desa		: ";cin>>kelurahan;
-	cout<<"Kecamatan		: ";cin>>kecamatan;
 	cout<<"Agama			: ";cin>>agama;
 	cout<<"Status Perkawinan	: ";getline(cin,status);
 	cout<<"Pekerjaan		: ";cin>>pekerjaan;
