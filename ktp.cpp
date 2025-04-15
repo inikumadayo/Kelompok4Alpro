15e44d9c596ebfca939fc7321ada87631ba8fc60
Sorashiro <rafisatyaprayoga@gmail.com>
2025-04-15 16:05:05 +0700

    Add files via upload

---
+++ ktp.cpp
@@ -0,0 +1,11 @@
+#include
+using namespace std;
+
+int main(){
+	long long int nik, tanggal,rt,rw;
+	string nama, tempat, kelamin, alamat, kelurahan, kecamatan, agama, status, pekerjaan, keluarga, berlaku hingga;
+	
+	cout<<"============KTP============"<<endl;
+	cout<<"Nama"
+	
+}
